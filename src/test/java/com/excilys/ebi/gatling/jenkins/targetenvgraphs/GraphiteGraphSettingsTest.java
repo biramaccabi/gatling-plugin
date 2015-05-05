a6896003219e4afcf2351aade5bd914dd8eef059
Wenxian Yang <wyang@shutterfly.com>
2015-05-05 16:31:19 -0700

    update new graphite sfly app server rule

--- src/test/java/com/excilys/ebi/gatling/jenkins/targetenvgraphs/GraphiteGraphSettingsTest.java
+++ src/test/java/com/excilys/ebi/gatling/jenkins/targetenvgraphs/GraphiteGraphSettingsTest.java
@@ -51,8 +51,8 @@ public class GraphiteGraphSettingsTest {
     @org.junit.Test
     public void testEquals() {
         String host = "http://tre-stats.internal.shutterfly.com";
-        String target = "sfly.foxtrot.host.app.*.GarbageCollectorSentinel.ParNew.heapUsagePercentage";
-        String title = "sfly.foxtrot.host.app.*.GarbageCollectorSentinel.ParNew.heapUsagePercentage";
+        String target = "sfly.foxtrot.host.app.*.app.GarbageCollectorSentinel.ParNew.heapUsagePercentage";
+        String title = "sfly.foxtrot.host.app.*.app.GarbageCollectorSentinel.ParNew.heapUsagePercentage";
         String percent_heap_used = "percent_heap_used";
         String yMax = "100";
         String yMin = "0";
